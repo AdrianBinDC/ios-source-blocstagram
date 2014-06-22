ecff6c79c08f80a48d8ac984ef16da77bdb4af1c
Aaron Brager <getaaron@gmail.com>
2014-06-22 17:22:39 -0500

    Building Blocstagram

---
+++ Blocstagram/Blocstagram/BLCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BLCAppDelegate.h
+//  Blocstagram
+//
+//  Created by Aaron Brager on 6/22/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Blocstagram/Blocstagram/BLCImagesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BLCImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Aaron Brager on 6/22/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCImagesTableViewController : UITableViewController
+
+@end
