05d6e8c14e7c39ed9a4656d568734104021c5921
Aaron Brager <getaaron@gmail.com>
2014-08-15 15:08:19 -0500

    Photo Filters and Posting to Instagram

---
+++ Blocstagram/BLCPostToInstagramViewController.h
@@ -0,0 +1,15 @@
+//
+//  BLCPostToInstagramViewController.h
+//  Blocstagram
+//
+//  Created by Aaron on 8/13/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCPostToInstagramViewController : UIViewController
+
+- (instancetype) initWithImage:(UIImage *)sourceImage;
+
+@end
